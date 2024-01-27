d0c201f1151fb8704ca9532c2255ddf0b46548ab
subayks <32360429+subayks@users.noreply.github.com>
2024-01-27 14:54:06 +0530

    UIpicker Added

--- Store Front-Sellacha/ItemList/View/ItemListViewController.swift
+++ Store Front-Sellacha/ItemList/View/ItemListViewController.swift
@@ -16,11 +16,14 @@ class ItemListViewController: UIViewController {
     @IBOutlet weak var itemListCollectionView: UICollectionView!
     var vm: ItemListViewModel?
     var isCartClicked:(()->())?
+    var filterPicker = UIPickerView()
+    var sortPicker = UIPickerView()
+    var brandPicker = UIPickerView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = false
-
+        
         self.buttonCart.layer.cornerRadius = 10
         self.overVieFilter.layer.cornerRadius = 10
         self.overViewSort.layer.cornerRadius = 10
@@ -35,6 +38,7 @@ class ItemListViewController: UIViewController {
         
         let button1 = UIBarButtonItem(image: UIImage(named: "Search"), style: .plain, target: self, action: #selector(searchClicked(tapGestureRecognizer:)))
         self.navigationItem.rightBarButtonItem  = button1
+        
     }
     
     func setupNavigationBar() {
@@ -53,12 +57,42 @@ class ItemListViewController: UIViewController {
     }
 
     @IBAction func actionFilter(_ sender: Any) {
+        filterPicker = UIPickerView.init()
+        filterPicker.delegate = self
+        filterPicker.dataSource = self
+        self.filterPicker.tag = 3
+        filterPicker.backgroundColor = UIColor.white
+        filterPicker.setValue(UIColor.black, forKey: "textColor")
+        filterPicker.autoresizingMask = .flexibleWidth
+        filterPicker.contentMode = .center
+        filterPicker.frame = CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 300)
+        self.view.addSubview(filterPicker)
     }
     
     @IBAction func actionSort(_ sender: Any) {
+        sortPicker = UIPickerView.init()
+        sortPicker.delegate = self
+        sortPicker.dataSource = self
+        self.sortPicker.tag = 2
+        sortPicker.backgroundColor = UIColor.white
+        sortPicker.setValue(UIColor.black, forKey: "textColor")
+        sortPicker.autoresizingMask = .flexibleWidth
+        sortPicker.contentMode = .center
+        sortPicker.frame = CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 300)
+        self.view.addSubview(sortPicker)
     }
     
     @IBAction func actionBrand(_ sender: Any) {
+        brandPicker = UIPickerView.init()
+        brandPicker.delegate = self
+        brandPicker.dataSource = self
+        self.brandPicker.tag = 1
+        brandPicker.backgroundColor = UIColor.white
+        brandPicker.setValue(UIColor.black, forKey: "textColor")
+        brandPicker.autoresizingMask = .flexibleWidth
+        brandPicker.contentMode = .scaleAspectFit
+        brandPicker.frame = CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 300)
+        self.view.addSubview(brandPicker)
     }
     
     @IBAction func actionCart(_ sender: Any) {
@@ -107,5 +141,51 @@ extension ItemListViewController : UICollectionViewDelegateFlowLayout{
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 10
+    }
+}
+
+extension ItemListViewController: UIPickerViewDelegate,UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView.tag {
+        case 1:
+            return self.vm?.brandArray.count ?? 0
+        case 2:
+            return self.vm?.sortArray.count ?? 0
+        case 3:
+            return self.vm?.filterArray.count ?? 0
+        default:
+            return 1
+        }
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+        case 1:
+            return self.vm?.brandArray[row]
+        case 2:
+            return self.vm?.sortArray[row]
+        case 3:
+            return self.vm?.filterArray[row]
+        default:
+            return "Data not found"
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch pickerView.tag {
+        case 1:
+            return self.brandPicker.isHidden = true
+        case 2:
+            return self.sortPicker.isHidden = true
+        case 3:
+            return self.filterPicker.isHidden = true
+        default:
+            return self.brandPicker.isHidden = true
+        }
     }
 }
--- Store Front-Sellacha/ItemList/ViewModel/ItemListViewModel.swift
+++ Store Front-Sellacha/ItemList/ViewModel/ItemListViewModel.swift
@@ -10,6 +10,10 @@ class ItemListViewModel: BaseViewModel {
     var dressArray: [DressCellObject]?
     var title: String?
     
+    var brandArray = ["Puma", "Adidas", "Polo", "LP"]
+    var sortArray = ["Low to High", "High to Low", "Discount"]
+    var filterArray = ["Men", "Women", "Kids"]
+    
     init(dressArray: [DressCellObject], title: String) {
         self.dressArray = dressArray
         self.title = title
